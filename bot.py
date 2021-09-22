61216efe0eb5bad00f0a3f284f785b6d3ea7ad64
MaxLewHL <88885132+MaxLewHL@users.noreply.github.com>
2021-09-22 19:32:13 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,41 @@
 import discord
 from discord.ext import commands
-from discord.flags import Intents
+import json
+import random
+import os
+
+with open('setting.json', 'r', encoding='utf8') as jFile:
+    jData = json.load(jFile)
 
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='>', intents = intents)
+bot = commands.Bot(command_prefix='>', intents=intents)
 
 @bot.event
 async def on_ready():
     print(">> bot online <<")
 
-@bot.event
-async def on_member_join(member):
-    channel = bot.get_channel(882177143411449900)
-    await channel.send(f'{member} 加入了本伺服器！')
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'cmds.{extension}')
+    await ctx.send(f'加载 {extension} 完毕。')
 
-@bot.event
-async def on_member_remove(member):
-    channel = bot.get_channel(882778508013756427)
-    await channel.send(f'{member} 离开了本伺服器qwq')
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cmds.{extension}')
+    await ctx.send(f'取消加载 {extension} 完毕。')
 
-# 还有一个bot.run因不想泄露BOT token而删除了
+@bot.command()
+async def reload(ctx, extension):
+    bot.reload_extension(f'cmds.{extension}')
+    await ctx.send(f'重新加载 {extension} 完毕。')
+
+
+
+for finename in os.listdir('./cmds'):
+    if finename.endswith('.py'):
+        bot.load_extension(f'cmds.{finename[:-3]}')
+
+if __name__ == "__main__":
+    bot.run(jData['TOKEN'])
